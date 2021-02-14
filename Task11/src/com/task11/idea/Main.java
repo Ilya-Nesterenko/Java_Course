f1a79a85d21cb73d345a9d48e9e5c990457b9827
Ilya-Nesterenko <derytery@yandex.ru>
2021-02-14 19:22:49 +0300

    Task11.1

--- Task11/src/com/task11/idea/Main.java
+++ Task11/src/com/task11/idea/Main.java
@@ -9,8 +9,10 @@ public class Main {
         String a = in.next();
         System.out.println("Второе первое число");
         int b = in.nextInt();
-        int res = Math.max(Integer.parseInt(a), b);
-        System.out.println(res);
+        int res1 = Math.max(Integer.parseInt(a), b);
+        System.out.println(res1);
+        double res2 = Math.min(Double.parseDouble(a), (double) b);
+        System.out.println(res2);
     }
 
 
